d8cd6648d93de96f59b3ad9ff670d40ee66d4f0d
Darius <martindarius01@gmail.com>
2022-10-26 14:46:04 -0400

    hey

---
+++ app.js
@@ -0,0 +1,65 @@
+// let dog = {
+ //    name:"Rambo",
+ //    numLegs:4
+ //  };
+
+ //  console.log(dog.name);
+ //  console.log(dog.numLegs);
+
+ //let dog = {
+ //   name: "Rambo",
+ //   numLegs: 4,
+ //   sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+ // };
+  
+  // dog.sayLegs();
+
+  // function Dog() {
+  //   this.name = "Rambo";
+  //   this.color = "black";
+  //   this.numLegs = 4;
+  // }
+
+  // function Dog() {
+  //   this.name = "Rambo";
+  //   this.color = "black";
+  //   this.numLegs = 4;
+  // }
+  // Only change code below this line
+  
+  // let hound = new Dog();
+
+  //function Dog() {
+  //  function Dog(name,color) {
+  //    this.name = Rambo;
+  //    this.color = Black;
+  //    this.numLegs = 4;
+  //  }
+  //  terrier.name
+  //  terrier.color
+  //  terrier.numLegs
+    
+  //  }
+
+  // function Dog(name) {
+  //   this.name = name;
+  //   this.numLegs = 4;
+  // }
+  
+  // let canary = new Dog("Rambo");
+  // let ownProps = [];
+  // Add your code below this line
+  // for (let property in canary) {
+  //   if (canary.hasOwnProperty(property)) {
+  //     ownProps.push(property);
+  //   }
+  // }
+
+ // function Dog(name) {
+ //   this.name = name;
+ // }
+  
+ // Dog.prototype.numLegs = 4;
+  
+  // Add your code above this line
+ // let beagle = new Dog("Snoopy");
